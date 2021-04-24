dd84ae468299b125e08cdb3e71a92198c5c3b4ec
Lucas Ferreira <lucas.ferreira057@bandtec.com.br>
2021-04-24 15:28:34 -0300

    Project generator script

---
+++ premake5.lua
@@ -0,0 +1,114 @@
+workspace "CrocEngine"
+	architecture "x64"
+	
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "CrocEngine"
+	location "CrocEngine"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files 
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs 
+	{
+	
+		"%{prj.name}/vendor/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "10.0.19041.0"
+
+		defines{
+			"CROC_PLATAFORM_WINDOWS",
+			"CR_BUILD_DLL"
+		}
+
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+		}
+	
+
+	filter "configurations:Debug"
+		defines "CR_DEBUG"
+		symbols "On"
+
+
+		
+	filter "configurations:Release"
+		defines "CR_RELEASE"
+		optimize "On"
+
+		
+	filter "configurations:Dist"
+		defines "CR_DIST"
+		optimize "On"
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+
+	language "C++"
+	
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files 
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+	
+		"CrocEngine/vendor/spdlog/include",
+		"CrocEngine/src"
+	}
+
+	links
+	{
+		"CrocEngine"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "10.0.19041.0"
+
+		defines{
+			"CROC_PLATAFORM_WINDOWS",
+		}
+	
+
+	filter "configurations:Debug"
+		defines "CR_DEBUG"
+		symbols "On"
+
+
+		
+	filter "configurations:Release"
+		defines "CR_RELEASE"
+		optimize "On"
+
+		
+	filter "configurations:Dist"
+		defines "CR_DIST"
+		optimize "On"
